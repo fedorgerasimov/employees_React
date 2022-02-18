a9468500c7c24a6a4f44e5035f237b6aca6c20d6
fedorgerasimov <1031577@mail.ru>
2022-02-18 19:58:51 +0300

    employees

---
+++ src/components/app-info/app-info.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './app-info.css'
+
+
+const AppInfo = () => {
+    return (
+        <div className='app-info'>
+            <h1> Accounting for employees in the company "DrumMaster </h1>
+            <h2>Total number of employees: </h2>
+            <h3>will receive an award</h3>
+        </div>
+    )
+}
+
+export  default AppInfo;
